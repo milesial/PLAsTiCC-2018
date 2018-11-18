01e6ace5e543560438a6f76fa3b38e7800a3daef
milesial <milesial@users.noreply.github.com>
2018-11-18 17:27:00 +0100

    Feature extraction with multiple workers for train and test

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -1,7 +1,25 @@
 import numpy as np
+import csv
+from tqdm import tqdm
 from cesium.featurize import featurize_time_series
+from multiprocessing import Manager
+from multiprocessing import Pool
 
 from utils.data_utils import get_train_objects
+from utils.data_utils import get_test_objects
+from utils.data_utils import get_num_test_shards
+from utils.constants import METADATA_COLUMNS
+from utils.constants import NUM_TEST_EXAMPLES
+from utils.constants import NUM_TRAIN_EXAMPLES
+
+FEATURES = [
+    'amplitude',
+    'median',
+    'median_absolute_deviation',
+    'minimum',
+    'maximum',
+    'all_times_nhist_numpeaks'
+]
 
 
 def get_features(time_series):
@@ -11,14 +29,7 @@ def get_features(time_series):
 
     features = featurize_time_series(
         times, values, errors,
-        features_to_use=[
-            'amplitude',
-            'median',
-            'median_absolute_deviation',
-            'minimum',
-            'maximum',
-            'all_times_nhist_numpeaks'
-        ]
+        features_to_use=FEATURES
     )
     return features.values
 
@@ -28,6 +39,7 @@ def featurize_train_object(time_series, metadata):
     features_vector = np.hstack(features)
     full_vector = np.hstack([features_vector, metadata.drop('target').values])
     full_vector[np.isnan(full_vector)] = 0
+    full_vector[np.isinf(full_vector)] = 0
     return full_vector, metadata['target']
 
 
@@ -36,10 +48,63 @@ def featurize_test_object(time_series, metadata):
     features_vector = np.hstack(features)
     full_vector = np.hstack([features_vector, metadata.values])
     full_vector[np.isnan(full_vector)] = 0
+    full_vector[np.isinf(full_vector)] = 0
     return full_vector
 
 
-def get_featurized_train_objects():
-    train_set = get_train_objects()
-    train_set = map(lambda o: featurize_train_object(o[1], o[2]), train_set)
-    return train_set
+def _featurize_train_fn(object):
+    return featurize_train_object(object[1], object[2])
+
+
+def get_featurized_train_objects(n_workers, verbose=False):
+    if verbose:
+        print('\nLoading train dataset...')
+
+    train_set = []
+    for o in tqdm(get_train_objects(), total=NUM_TRAIN_EXAMPLES):
+        train_set.append(o)
+
+    if verbose:
+        print('\nComputing features...')
+        bar = tqdm(total=NUM_TRAIN_EXAMPLES)
+
+    chunksize = int(NUM_TRAIN_EXAMPLES / n_workers)
+    with Pool(n_workers) as pool:
+        for out in pool.imap(_featurize_train_fn, train_set, chunksize):
+            if verbose:
+                bar.update()
+            yield out
+
+
+def _featurize_shard(queue, shard_id):
+    for oid, ts, meta in get_test_objects(shard_id):
+        feature = featurize_test_object(ts, meta)
+        queue.put([oid, *feature])
+
+
+def featurize_and_save_test_set(n_workers, out_csv):
+    pbar = tqdm(total=NUM_TEST_EXAMPLES)
+    header = ['object_id', *FEATURES, *METADATA_COLUMNS[:-1]]
+    n_shards = get_num_test_shards()
+    chunksize = int(n_shards / n_workers)
+    if chunksize < 1:
+        chunksize = 1
+
+    m = Manager()
+    q = m.Queue()
+    with Pool(n_workers) as pool:
+        r = pool.starmap_async(_featurize_shard, [(q, i) for i in range(n_shards)], chunksize=1)
+
+        with open(out_csv, 'w') as f:
+            writer = csv.writer(f, lineterminator='\n')
+            writer.writerow(header)
+            while True:
+                item = q.get(block=True)
+                writer.writerow(item)
+                pbar.update(1)
+
+        r.wait()
+
+
+if __name__ == '__main__':
+    featurize_and_save_test_set(10, '../data/test_set_features.csv')
--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,5 +1,21 @@
 CLASSES = [6, 15, 16, 42, 52, 53, 62, 64, 65, 67, 88, 90, 92, 95, 99]
 
 NUM_TRAIN_EXAMPLES = 7848
+NUM_TEST_EXAMPLES = 3492890
 
 NUM_TEST_ROWS = 453653104
+
+METADATA_COLUMNS = [
+    'object_id',
+    'ra',
+    'decl',
+    'gal_l',
+    'gal_b',
+    'ddf',
+    'hostgal_specz',
+    'hostgal_photoz',
+    'hostgal_photoz_err',
+    'distmod',
+    'mwebv',
+    'target'
+]
