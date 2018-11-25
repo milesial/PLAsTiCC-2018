Fixed worker hang on feature calculation

src/feature_extraction.py
@@ -29,28 +29,9 @@ FEATURES = [
     'percent_difference_flux_percentile',
     'skew',
     'std',
-    'freq1_amplitude1',
-    'freq1_amplitude2',
-    'freq1_amplitude3',
-    'freq1_freq',
-    'freq1_lambda',
-    'freq1_rel_phase2',
-    'freq1_rel_phase3',
-    'freq1_rel_phase4',
-    'freq1_signif',
-    'freq2_freq',
-    'freq3_freq',
-    'freq_y_offset',
-    'fold2P_slope_10percentile',
-    'fold2P_slope_90percentile',
     'freq1_freq',
     'freq1_lambda',
     'freq_varrat',
-    'linear_trend',
-    'freq_signif_ratio_21',
-    'freq_signif_ratio_31',
-    'stetson_j',
-    'stetson_k'
 ]
 
 
@@ -69,18 +50,18 @@ def get_features(time_series):
 def featurize_train_object(time_series, metadata):
     features = get_features(time_series)
     features_vector = np.hstack(features)
-    full_vector = np.hstack([features_vector, metadata.drop('target').values])
+    full_vector = np.hstack([features_vector, metadata.drop('target').values]).astype(np.float32)
     full_vector[np.isnan(full_vector)] = 0
     full_vector[np.isinf(full_vector)] = 0
-    return full_vector, np.asarray(metadata['target'])
+    return full_vector, np.asarray(metadata['target'], np.float32)
 
 
 def featurize_test_object(time_series, metadata):
     features = get_features(time_series)
     features_vector = np.hstack(features)
-    full_vector = np.hstack([features_vector, metadata.values])
+    full_vector = np.hstack([features_vector, metadata.values]).astype(np.float32)
     full_vector[np.isnan(full_vector)] = 0
-    full_vector[not np.isfinite(full_vector)] = 0
+    full_vector[np.isinf(full_vector)] = 0
     return full_vector
 
 
@@ -117,7 +98,7 @@ def _featurize_test_shard(queue, shard_id):
 def featurize_and_save_test_set(n_workers, out_csv):
     pbar = tqdm(total=NUM_TEST_EXAMPLES)
     features = [f'band{b}_{f}' for b in range(NUM_PASSBANDS) for f in FEATURES]
-    header = ['object_id', *features, *METADATA_COLUMNS[:-1]]
+    header = ['object_id', *features, *METADATA_COLUMNS[1:-1]]
     n_shards = get_num_test_shards()
     chunksize = int(n_shards / n_workers)
     if chunksize < 1:
@@ -126,7 +107,7 @@ def featurize_and_save_test_set(n_workers, out_csv):
     m = Manager()
     q = m.Queue()
     with Pool(n_workers) as pool:
-        r = pool.starmap_async(_featurize_test_shard, [(q, i) for i in range(n_shards)], chunksize=1)
+        r = pool.starmap_async(_featurize_test_shard, [(q, i) for i in range(n_shards)], chunksize=chunksize)
 
         with open(out_csv, 'w') as f:
             writer = csv.writer(f, lineterminator='\n')
@@ -141,7 +122,7 @@ def featurize_and_save_test_set(n_workers, out_csv):
 
 def featurize_and_save_train_set(n_workers, out_csv):
     features = [f'band{b}_{f}' for b in range(NUM_PASSBANDS) for f in FEATURES]
-    header = ['object_id', *features, *METADATA_COLUMNS]
+    header = ['object_id', *features, *METADATA_COLUMNS[1:]]
 
     with open(out_csv, 'w') as f:
         writer = csv.writer(f, lineterminator='\n')
@@ -152,4 +133,4 @@ def featurize_and_save_train_set(n_workers, out_csv):
 
 
 if __name__ == '__main__':
-    featurize_and_save_test_set(4, '../data/test_set_features.csv')
+    featurize_and_save_test_set(8, '../data/test_set_features.csv')